e0243ed4b04616d35a5e5364a58448a1f562d901
mgas90 <asmg90@gmail.com>
2021-06-07 20:33:29 -0300

    implemntación del eliminar y local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import React,{ Fragment, useState,useRef} from  'react';//useRef hace referencia al eelemento 
+import React,{ Fragment, useState, useRef,useEffect } from  'react';//useRef hace referencia al eelemento 
 import { TodoList } from './components/TodoList';
 import { v4 as uuidv4 } from 'uuid'
 
 export function App(){
   const [todos,setTodos] = useState([
-    {id:1, task:"tarea-1", complete: false}
+    {id:1, task:"tarea-1", completed: false}
   ])
-  //para poder usar el useRef hay que declararlo
+ const KEY = "todoApp.todos";
+  //locaStorage: nos va a permitir guardar las tareas aun que refresquemos 
+
+//useEffect:es usado para actualizaciones en el DOM
+useEffect(() => {
+  const storedTodos = JSON.parse(localStorage.getItem(KEY));
+  if(storedTodos){
+    setTodos(storedTodos)
+  }
+
+}, [])
+//llammos a useEffect que recibe una funcion de callback y array de dpendncia y si quieres que se ejecute constantemente colocas ahi las dependencias que quieres que se escuche
+useEffect(() => {
+  //localsstora solo almacena string
+  localStorage.setItem(KEY, JSON.stringify(todos))
+
+}, [todos])
+  
+//para poder usar el useRef hay que declararlo
   const todoTaskRef = useRef()
 
 const toggleTodo = (id) =>{
   const newTodos = [...todos];
   const todo = newTodos.find((todo)=> todo.id === id);
-  todo.completed = !todo.complete;
+  todo.completed = !todo.completed;
   setTodos(newTodos)
 }
 const handleTodoAdd= () =>{
@@ -26,15 +44,21 @@ const handleTodoAdd= () =>{
     })
 
     todoTaskRef.current.value = null
-  }
-  
+}
+//elimina las tareas 
+const handleClearAll = () =>{
+  const newTodos = todos.filter((todo) => !todo.completed)
+  setTodos(newTodos);
+}
+
+
   return (
     <Fragment>
       <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea"/>
       <button onClick={handleTodoAdd}>+</button>
-      <button>-</button>
-      <div>Te quedan {todos.filter((todo)=>!todo.complete).length} tareas por terminar</div>
+      <button onClick={handleClearAll}>-</button>
+      <div>Te quedan {todos.filter((todo)=>!todo.completed).length} tareas por terminar</div>
     </Fragment>
   )
 }
